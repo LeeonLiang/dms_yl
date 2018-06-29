e932f170fdc6e6274e4d04848e06bf8318eb90e6
leeon <leeon_liang@foxmail.com>
2018-06-29 14:05:17 +0800

    add test util

--- application/demo/controller/Index.php
+++ application/demo/controller/Index.php
@@ -1,12 +1,24 @@
 <?php
 namespace app\demo\controller;
 
+use app\demo\util\Common;
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
     public function index()
     {
         return $this->fetch();
+    }
+
+    public function getJsonIndex()
+    {
+        return json(Db::query('select * from think_user'));
+    }
+
+    public function testUtil()
+    {
+        return json(Common::test());
     }
 }
---
+++ application/demo/util/Common.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\demo\util;
+
+class Common {
+
+    public static function test()
+    {
+        return 'test';
+    }
+
+}
+
