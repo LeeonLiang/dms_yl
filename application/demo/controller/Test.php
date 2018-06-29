23aa2725e365d0905e980111e9d8bcbbb7d48bb6
leeon <leeon_liang@foxmail.com>
2018-06-29 17:10:44 +0800

    add test db model

--- application/demo/controller/Test.php
+++ application/demo/controller/Test.php
@@ -1,7 +1,44 @@
 <?php
 namespace app\demo\controller;
 
-class Test
+use think\Controller;
+use think\Request;
+
+class Test extends Controller
 {
 
+    function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+    }
+
+    /**
+     * 测试Model Insert
+     * @throws \Exception
+     */
+    public function testDbInsert()
+    {
+        $objModelTest = new \app\demo\model\Test();
+        $arrTestList = [
+            ['name' => 'test1'],
+            ['name' => 'test2']
+        ];
+        $mixRes = $objModelTest->saveAll($arrTestList);
+        return json($mixRes);
+    }
+
+    /**
+     * 测试Model Select One
+     * @throws \think\exception\DbException
+     */
+    public function testDbSelectOne()
+    {
+        $objModelTest = \app\demo\model\Test::get(1);
+        return json($objModelTest);
+    }
+
+    public function testDbSelectAll()
+    {
+       
+    }
 }
